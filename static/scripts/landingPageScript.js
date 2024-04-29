e046bccc17531aa0368920cc5bf0a6727f2a21eb
DanielMichel305 <daniel2202556@miuegypt.edu.eg>
2024-04-29 16:17:00 +0300

    Finished search bar on landing page

---
+++ static/scripts/landingPageScript.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById("searchBar-search-sb").addEventListener("focus", function() {
+        document.getElementById('searchBar-container').style.boxShadow= "0 0 5px #27cbff";
+    });
+    document.getElementById("searchBar-search-sb").addEventListener("blur", function() {
+        document.getElementById('searchBar-container').style.boxShadow= "none";
+    });
+});
+
