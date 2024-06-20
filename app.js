c300c83e7f156dd3a3a1c52d9652a6398abf5e16
DanielMichel305 <daniel2202556@miuegypt.edu.eg>
2024-06-20 15:29:41 +0300

    Implemented HTTPS server (Certs not included -find on WP/DISC-)

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ const session = require('express-session')
 const passport = require('passport');
 const mongoose = require('mongoose');
 const path = require("path");
+const http = require('http');
+const https = require('https');
+const fs = require('fs');
 
 const app = express();
 const port = 5000;
@@ -17,6 +20,11 @@ app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
 app.use(express.json());       // to support JSON-encoded bodies
 app.use(express.urlencoded()); // to support URL-encoded bodies
 
+
+const privateKey = fs.readFileSync('./certs/key.pem', 'utf8');
+const certificate = fs.readFileSync('./certs/cert.pem', 'utf8');
+
+const credentials = { key: privateKey, cert: certificate };
 
 
 const authRouter = require('./Routes/authRoutes');
@@ -66,28 +74,6 @@ app.get("/register", (req, res) => {
   res.render("register");
 });
 
-/* app.post('/register', (req,res) => {
-
-  var User = require('./Model/UserModel');
-  bcrypt.genSalt(10,(err,salt) => {
-    bcrypt.hash(req.body.password, salt , (err, hash) =>{
-       if(err) throw (err);
-
-       //console.log(hash);
-       var user = new User({email: req.body.email, firstName: req.body.name, lastName: req.body.lastname, auth_Method: true, password: hash, activated: false});
-       console.log(user);
-       user.save().then(item =>{
-         res.send("Information saved to database");
-       })
-       .catch(err =>{
-         res.status(400).send("Unable to save to database");
-       });     
-    });
-  });
-
-}); */
-
-
 app.get("/clientprofile", (req, res) => {
   res.render("clientprofile");
 });
@@ -110,6 +96,11 @@ app.get('/protected',isLoggedIn ,(req,res)=>{
   res.send("Protected Page");
 
 })
+
+const httpsServer = https.createServer(credentials, app);
+httpsServer.listen(443, () => {
+  console.log('HTTPS Server running on port 443');
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
