66f23465c9d49770940b5e79b7a7009b43f7797e
Abdelrahman Osama <116208627+AbdElRahman1234567890@users.noreply.github.com>
2024-06-23 03:06:48 +0300

    Add files via upload

--- static/scripts/clienthistory.js
+++ static/scripts/clienthistory.js
@@ -1,56 +1,56 @@
 const orders = [
-    {
-      imageUrl: "images/jobs/webDev.jpeg",
-      type: "Programming & Tech",
-      link: "Web Development",
-      
-    },
-    {
-      imageUrl: "images/jobs/audio1.jpeg",
-      type: "Programming & Tech",
-      link: "Web Development",
-    },
-    {
-      imageUrl: "images/jobs/webDev.jpeg",
-      type: "Programming & Tech",
-      link: "Web Development",
-    }
-  ];
-  
+  {
+    imageUrl: "images/jobs/webDev.jpeg",
+    type: "Programming & Tech",
+    link: "Web Development",
+
+  },
+  {
+    imageUrl: "images/jobs/audio1.jpeg",
+    type: "Programming & Tech",
+    link: "Web Development",
+  },
+  {
+    imageUrl: "images/jobs/webDev.jpeg",
+    type: "Programming & Tech",
+    link: "Web Development",
+  }
+];
+
 
 
 
 
 
 function createOrderCard(order) {
-    const cardHTML = `
-      <div class="orders-card">
-        <div class="images-div">
-          <img src="${order.imageUrl}">
-        </div>
-        <div class="cardType">
-          <p id="cardtxt">type:</p>
-          <p>${order.type}</p>
-        </div>
-        <div class="cardText">
-          <a href="#" class="acard">${order.link}</a>
-          </div>
+  const cardHTML = `
+    <div class="orders-card">
+      <div class="images-div">
+        <img src="${order.imageUrl}">
+      </div>
+      <div class="cardType">
+        <p id="cardtxt">type:</p>
+        <p>${order.type}</p>
+      </div>
+      <div class="cardText">
+        <a href="#" class="acard">${order.link}</a>
         </div>
       </div>
-      <hr>
-    `;
-    return cardHTML;
-  }
+    </div>
+    <hr>
+  `;
+  return cardHTML;
+}
 
-   
+
 function renderOrderCards() {
-    const prevOrdersDiv = document.getElementById("prev-orders");
-    prevOrdersDiv.innerHTML = "";
-  
-    orders.forEach(order => {
-      const orderCard = createOrderCard(order);
-      prevOrdersDiv.innerHTML += orderCard;
-    });
-  }
+  const prevOrdersDiv = document.getElementById("prev-orders");
+  prevOrdersDiv.innerHTML = "";
 
-  renderOrderCards();
+  orders.forEach(order => {
+    const orderCard = createOrderCard(order);
+    prevOrdersDiv.innerHTML += orderCard;
+  });
+}
+
+renderOrderCards();
